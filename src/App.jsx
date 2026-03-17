0529dcc04097f284ac3d977fe06683d21bd5bb1d
Shashank Mandoli <shashankmandoli2004@gmail.com>
2026-03-18 00:43:20 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <>
+      <h1 className='bg-gray-700 text-white text-2xl text-center'>Hello World !!!</h1>
+    </>
+  )
+}
+
+export default App
